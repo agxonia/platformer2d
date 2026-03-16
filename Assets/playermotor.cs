8064922ff711cf6956563285824810f6c2d35436
agxonia <dsw63516@student.dsw.edu.pl>
2026-03-16 10:41:07 +0100

    motor

---
+++ Assets/playermotor.cs
@@ -0,0 +1,28 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playermotor : MonoBehaviour
+{
+    Vector2 direction;
+    public float speed = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+    }
+  void OnMove(InputValue value)
+    {
+        Debug.Log("Move");
+        //Debug.Log(value.Get<Vector2>());
+        direction = value.Get<Vector2>();
+    }
+
+}
+
